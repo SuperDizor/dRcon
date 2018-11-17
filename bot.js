4bca9617fee3bb4b79005bc7d0b14eb6fe287004
SuperDizor <felixdion25@hotmail.com>
2018-11-17 03:55:09 -0500

    Final V1

--- bot.js
+++ bot.js
@@ -2,54 +2,103 @@ const Discord = require("discord.js");
 const Gamedig = require('gamedig');
 const client = new Discord.Client();
 const settings = require('./config.json');
-IsOnline = false;
-var timestamp = "\x1b[44m\x1b[31m[" + new Date().toLocaleTimeString('en-CA') + "]\x1b[0m ";
+
+var cRed = "\x1b[31m";
+var cBlue = "\x1b[34m";
+var cYellow = "\x1b[33m";
+var cGreen = "\x1b[32m";
+var cCyan = "\x1b[36m";
+var cNormal = "";
+var cReset = "\x1b[0m"; // DON'T FORGET RESET FONT
 
 configType = settings.type;
 configIp = settings.ip;
+IsOnline = false;
+var versionStart = "(v000.001)";
+var versionRefresh = "(v000.000)";
 
-function printLog(text) {
-  logText = text;
-  console.log(timestamp + text);
+Gamedig.query({
+  type: configType,
+  host: configIp
+}).then((stateo) => {
+  serverInfo = stateo;
+}).catch((error) => {
+
+  client.user.setActivity('Server Offline', {type: 'WATCHING'});
+
+});
+
+String.prototype.toHHMMSS = function () {
+    var sec_num = parseInt(this, 10); // don't forget the second param
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    return hours+':'+minutes+':'+seconds;
 }
 
-async function statusLoop(state) {
+function printLog(text, color) {
+  if (color === undefined) color = cNormal;
+  var timestamp = "[" + new Date().toLocaleTimeString('en-CA') + "] ";
+  colorLog = color;
+  logText = text;
+  console.log(timestamp + colorLog + text + cReset);
+}
+
+
+async function statusLoop(state) { //neeeeeeeeeedd work !!!!!!!!!!!
   stateF = state;
+  serverInfo = stateF;
 
-  client.user.setActivity('!ark | Server Online', {
+  client.user.setActivity('!ark | Server Online', {type: 'PLAYING'});
+
+  await sleep(4000);
+  //var playerConnected = '!ark | Players: ' + stateF.raw.numplayers + ' / ' + stateF.maxplayers;
+  client.user.setActivity('!ark | Players: ' + stateF.raw.numplayers + ' / ' + stateF.maxplayers, {
     type: 'PLAYING'
   });
-  //printLog('[Status Update] Server ONLINE');
 
-  await sleep(2000);
-  var playerConnected = '!ark | Players: ' + stateF.raw.numplayers + ' / ' + stateF.maxplayers;
-  client.user.setActivity(playerConnected, {
-    type: 'PLAYING'
-  });
-  //printLog('[Status Update] Player(s) Online');
+  await sleep(5000);
+  //var playerConnected = 'Host: ' + stateF.query.host;
+  client.user.setActivity('Host: ' + stateF.query.host, {type: 'PLAYING'});
 
-  await sleep(8000);
-  var playerConnected = 'Host: ' + stateF.query.host;
-  client.user.setActivity(playerConnected, {
-    type: 'PLAYING'
-  });
-  //printLog('[Status Update] Host: ' + stateF.query.host);
+  // ICI LE CODE POUR LA VERSION
+  await sleep(5000);
+  nameComplete = serverInfo.name;
+  //console.log(nameComplete);
+  var versionRefresh = nameComplete.slice(-10);
+  //console.log(version);
+  //console.log(versionStart);
+  //console.log(versionRefresh);
+  if (versionRefresh !== versionStart){
+  versionStart = versionRefresh;
+    printLog("Version: " + versionRefresh, cCyan);
+  }
+  //printLog("Version:" + version);
+  //var playerConnected = 'Version: WIP';
+  client.user.setActivity('Version: ' + versionRefresh, {type: 'PLAYING'});
 
-  await sleep(4500);
-  var playerConnected = 'Map: ' + stateF.map;
-  client.user.setActivity(playerConnected, {
-    type: 'PLAYING'
-  });
-  //printLog('[Status Update] Map: ' + stateF.map);
+  await sleep(5000);
+  //var playerConnected = 'Name: ' + stateF.name;
+  client.user.setActivity(serverInfo.name, {type: 'PLAYING'});
+
+  await sleep(5000);
+  //var playerConnected = 'Map: ' + stateF.map;
+  client.user.setActivity('Map: ' + stateF.map, {type: 'PLAYING'});
+
 }
 
 function sleep(ms) {
   return new Promise(resolve => {
-    setTimeout(resolve, ms)
-  })
+    setTimeout(resolve, ms);
+  });
 }
 
 function gamedigQuery() {
+  //printLog("gamedigQuery Started");
   IsOnline = false;
   Gamedig.query({
     type: configType,
@@ -58,93 +107,107 @@ function gamedigQuery() {
     IsOnline = true;
     statusrefresh(); // STATUS UPDATED - ONLINE
     statusLoop(state);
-
-
-
-
-    printLog('Player(s) connected: \x1b[33m' + state.raw.numplayers + '/' + state.maxplayers + '\x1b[0m');
+    playersLists();
+    //printLog('Player(s) connected: ' + state.raw.numplayers + ' / ' + state.maxplayers);
 
   }).catch((error) => { //--------------------------------------------------------OFFLINE---------------------
     // update discord bot status + playing string "Server Offline"
     statusrefresh(); // STATUS UPDATED - OFFLINE
 
-    status = 'Server Offline';
-    client.user.setActivity(status, {
-      type: 'WATCHING'
-    });
-    printLog("Server is offline");
+    //status = 'Server Offline';
+    client.user.setActivity('Server Offline', {type: 'WATCHING'});
+    printLog("Server is offline", cRed);
 
   });
 }
 
 function statusrefresh() {
-  if (IsOnline === true) {
-    client.user.setStatus('online');
-  } else {
-    client.user.setStatus('dnd');
-
+  try {
+    if (IsOnline === true) {
+      client.user.setStatus('online');
+    } else {
+      client.user.setStatus('dnd');
+    }
+  } catch (error) {
+    console.error(error);
   }
-
 }
 
-
+function playersLists(){
+  printLog('Player(s) connected: ' + serverInfo.raw.numplayers + ' / ' + serverInfo.maxplayers, cYellow);
+  for (var i = 0; i < serverInfo.players.length; i++) {
+      var counter = serverInfo.players[i];
+      num = i + 1;
+      //var timeH = counter.time.toHHMMSS();
+      printLog(num + ' - ' + counter.name, cGreen);
+  }
+}
 
 client.on('ready', () => {
-  client.user.setStatus('idle');
-  printLog("Bot Starting...");
-  client.user.setActivity('JavaScript while starting...', {
-    type: 'PLAYING'
-  });
-  printLog('\x1b[32mBot has started, with ' + client.users.size + ' users, in ' + client.channels.size + ' channels of ' + client.guilds.size + ' guilds.\x1b[0m')
-  //console.log('\x1b[32m%s\x1b[0m', `Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-  printLog('IP: \x1b[36m' + configIp + '\x1b[0m');
-  printLog('Type: \x1b[36m' + configType + '\x1b[0m');
-  client.setInterval(gamedigQuery, 20010);
-
+  try {
+    client.user.setStatus('idle');
+    client.user.setActivity('JavaScript while starting...', {type: 'PLAYING'});
+    printLog("Bot Starting...",cRed);
+    console.log(serverInfo);
+    printLog('Bot has started, with ' + client.users.size + ' users, in ' + client.channels.size + ' channels of ' + client.guilds.size + ' guilds.', cGreen);
+    printLog('IP: ' + configIp + '  |  Type: ' + configType, cCyan);
+    printLog('Server Name: ' + serverInfo.name, cBlue);
+    //playersLists();
+    client.setInterval(gamedigQuery, 30100);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 
 client.on("message", (message) => {
   if (message.content.startsWith("ping")) {
     message.channel.send("pong!");
-    update();
+    //update();
   }
 
   const lowm = "ark";
   const capsm = "ARK";
   const capsmo = "Ark";
 
-  if (message.content.includes(lowm) || message.content.includes(capsm) || message.content.includes(capsmo)) {
-    message.react(client.emojis.get('319703910002589696')) //SuperDizorTestServer
-      //message.react(client.emojis.get('508099744870957086')) // loungeServer
-      .then(reaction => printLog("Ark Emojis Added."));
-  }
+  try {
 
-  const ark = "!ark";
-  if (message.content.includes(ark)) {
-    message.react(client.emojis.get('319703910002589696')) //SuperDizorTestServer
-    message.channel.send({
-      "embed": {
-        "title": "ARK Server Status",
-        "color": 1234556,
-        "footer": {
-          "text": "Discord Bot created by SuperDizor"
-        },
-        "fields": [{
-            "name": "Server Name",
-            "value": "Currently Unavailable."
+    if (message.content.includes(lowm) || message.content.includes(capsm) || message.content.includes(capsmo)) {
+      message.react(client.emojis.get('319703910002589696')); //SuperDizorTestServer
+      //message.react(client.emojis.get('508099744870957086')) // loungeServer
+      //.then(reaction => printLog("Ark Emojis Added."));
+    }
+    //const ark = "!ark";
+    if (message.content.includes("!ark")) {
+      message.react(client.emojis.get('319703910002589696'));
+      message.channel.send({
+        "embed": {
+          "title": "ARK Server Status",
+          "color": 1234556,
+          "footer": {
+            "text": "Discord Bot created by SuperDizor"
           },
-          {
-            "name": "Host",
-            "value": "cyber-ray.ddns.net"
-          }
-        ]
-      }
-    });
+          "fields": [{
+              "name": "Server Name",
+              "value": serverInfo.name
+            },
+            {
+                "name": "Player(s) Online",
+                "value": serverInfo.raw.numplayers + ' / ' + serverInfo.maxplayers
+              },
+            {
+              "name": "Host + Port",
+              "value": serverInfo.query.host + ':' + serverInfo.query.port
+
+            }
+          ]
+        }
+      });
+    }
+
   }
+  catch(error) {console.error(error);}
 
 });
-
-
 
 client.login(settings.token);
