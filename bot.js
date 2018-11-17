3a5e0f048f3877d1f5b792753b557319c7c86726
SuperDizor <felixdion25@hotmail.com>
2018-11-17 04:26:38 -0500

    Timer Added

--- bot.js
+++ bot.js
@@ -138,9 +138,33 @@ function playersLists(){
   for (var i = 0; i < serverInfo.players.length; i++) {
       var counter = serverInfo.players[i];
       num = i + 1;
-      //var timeH = counter.time.toHHMMSS();
-      printLog(num + ' - ' + counter.name, cGreen);
+      var timeH = counter.time;
+      timer = seconds2time(timeH);
+      printLog(num + ' - ' + counter.name + ' | Timer: ' + timer, cGreen);
   }
+}
+
+function seconds2time (seconds) {
+    var seconds = Math.trunc(seconds);
+    var hours   = Math.floor(seconds / 3600);
+    var minutes = Math.floor((seconds - (hours * 3600)) / 60);
+    var seconds = seconds - (hours * 3600) - (minutes * 60);
+    var time = "";
+
+    if (hours != 0) {
+      time = hours+":";
+    }
+    if (minutes != 0 || time !== "") {
+      minutes = (minutes < 10 && time !== "") ? "0"+minutes : String(minutes);
+      time += minutes+":";
+    }
+    if (time === "") {
+      time = seconds+"s";
+    }
+    else {
+      time += (seconds < 10) ? "0"+seconds : String(seconds);
+    }
+    return time;
 }
 
 client.on('ready', () => {
